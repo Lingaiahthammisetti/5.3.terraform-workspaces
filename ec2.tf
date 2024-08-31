a401e956d6eb3694fa4b8f957ab8e6c86f2a6e38
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-31 20:06:27 +0530

    terraform msg

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
 
 resource "aws_instance" "workspace" {
   ami           = "ami-041e2ea9402c46c32"
-  vpc_security_group_ids = ["sg-0b35c64f66b53871f"]
+  vpc_security_group_ids =  ["sg-0b8c28fdd8f99cc89"]
   instance_type = lookup(var.instance_type,terraform.workspace)
+
+   tags = {
+    Name = "Terraform workspace" #Here 'Helloworld-db' is the ec2-instance name in AWS, not related to Terrafrom.
+  }
 }
 
